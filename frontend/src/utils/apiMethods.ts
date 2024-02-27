aa32efe6793e14111b7d67029dd35264bfbc8d47
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2024-02-28 00:06:03 +0530

    feat: api methods and coinbase added

---
+++ frontend/src/utils/apiMethods.ts
@@ -0,0 +1,79 @@
+import CryptoJS from "crypto-js";
+
+export const getProducts = async (
+  shopURL: string,
+  consumerKey: string,
+  consumerSecret: string
+) => {
+  try {
+    const response = await fetch(`${shopURL}/wp-json/wc/v3/products`, {
+      method: "GET",
+      headers: {
+        Authorization:
+          "Basic " +
+          Buffer.from(`${consumerKey}:${consumerSecret}`).toString("base64"),
+        "Content-Type": "application/json", // Adjust content type as needed
+        // Add other headers if required
+      },
+    });
+
+    console.log(response);
+    const products = (await response.json()).data;
+    return products;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const storeCredentialsLocal = async (
+  consumerKey: string,
+  consumerSecret: string
+) => {
+  try {
+    const encryptedKey = CryptoJS.AES.encrypt(consumerKey, "").toString();
+    localStorage.setItem("WooConsumerKey", encryptedKey);
+
+    const encryptedSecret = CryptoJS.AES.encrypt(consumerSecret, "").toString();
+    localStorage.setItem("WooConsumerSecret", encryptedSecret);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getCredentialsLocal = async (): Promise<
+  | {
+      consumerKey: string;
+      consumerSecret: string;
+    }
+  | undefined
+> => {
+  try {
+    const encryptedKeyFromStorage = localStorage.getItem("WooConsumerKey");
+    if (!encryptedKeyFromStorage) {
+      console.log("Key not found");
+      return;
+    }
+    const consumerKey = CryptoJS.AES.decrypt(
+      encryptedKeyFromStorage,
+      ""
+    ).toString(CryptoJS.enc.Utf8);
+
+    const encryptedSecretFromStorage =
+      localStorage.getItem("WooConsumerSecret");
+    if (!encryptedSecretFromStorage) {
+      console.log("Secret not found");
+      return;
+    }
+    const consumerSecret = CryptoJS.AES.decrypt(
+      encryptedSecretFromStorage,
+      ""
+    ).toString(CryptoJS.enc.Utf8);
+
+    return {
+      consumerKey,
+      consumerSecret,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+};
