302b1834a42519369d0a4c807178bbaab7728705
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2024-02-29 17:24:42 +0530

    feat: empty state and ui fixes

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import AddStore from "./add-store";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div className="flex items-center justify-between py-4 border-neutral-400 border-b">
-      <h1>Woocom Frames</h1>
+    <div className="flex items-center px-20 justify-between py-4 border-neutral-400 border-b">
+      <Link href={"/"} className=" text-xl font-semibold">
+        Woocom Frames
+      </Link>
       <AddStore />
     </div>
   );
