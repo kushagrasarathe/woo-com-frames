03c73c0ae85476a3645fc20a54bc79444fed9baf
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2024-02-29 20:25:28 +0530

    fix: checkKeys removed from navbar

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -8,7 +8,7 @@ export default function Navbar() {
       <Link href={"/"} className=" text-xl font-semibold">
         Woocom Frames
       </Link>
-      <AddStore />
+      {/* <AddStore /> */}
     </div>
   );
 }
